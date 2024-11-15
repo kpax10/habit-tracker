a62b04599e63b9b0f33f60cff4fd7b13d95aa04e
kyle <notxap55@yahoo.com>
2024-11-15 15:41:50 -0800

    completed

--- main.py
+++ main.py
@@ -34,17 +34,38 @@ graph_endpoint = f'{pixela_endpoint}/{USERNAME}/graphs'
 # response = requests.post(url=graph_endpoint, json=graph_config, headers=headers)
 #
 # print(response.text)
-today = datetime.today().strftime('%Y%m%d')
-pixel_params = {
-    'date': '20241113',
-    'quantity': '1',
-}
+
+
+# today = datetime.today().strftime('%Y%m%d')
+# pixel_params = {
+#     'date': '20241113',
+#     'quantity': '1',
+# }
+#
+# headers = {
+#     'X-USER-TOKEN': TOKEN,
+# }
+#
+# pixel_creation_endpoint = f"{pixela_endpoint}/{USERNAME}/graphs/{GRAPH_ID}"
+#
+# response = requests.post(url=pixel_creation_endpoint, json=pixel_params, headers=headers)
+# print(response)
+
+pixel_update_endpoint = f"{pixela_endpoint}/{USERNAME}/graphs/{GRAPH_ID}/20241115"
 
 headers = {
     'X-USER-TOKEN': TOKEN,
 }
 
-pixel_creation_endpoint = f"{pixela_endpoint}/{USERNAME}/graphs/{GRAPH_ID}"
+update_params = {
+    'quantity': '0'
+}
 
-response = requests.post(url=pixel_creation_endpoint, json=pixel_params, headers=headers)
-print(response)
+# response = requests.put(url=pixel_update_endpoint, json=update_params, headers=headers)
+# print(response)
+
+delete_update_endpoint = f"{pixela_endpoint}/{USERNAME}/graphs/{GRAPH_ID}/20241115"
+
+
+response = requests.delete(url=delete_update_endpoint, headers=headers)
+print(response.text)
